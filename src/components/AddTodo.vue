fbc9684cc9067a5f5759de7c3a411344b7a6d877
Thang La <thangla@Thangs-MacBook-Pro.local>
2020-04-12 10:48:19 +0700

    them sua xoa

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <label>Thêm vầo</label>
+        <div style="display:flex;flex-flow:row nowrap">
+            <div>
+                <label>Fist name</label>
+                <input v-model="input.firstname" type="text" />
+            </div>
+            <div>
+                <label>Last name</label>
+                <input v-model="input.lastname" type="text" />
+            </div>
+            <div>
+                <label>Email name</label>
+                <input v-model="input.email" type="text" />
+            </div>
+            <div>
+                <label>Code name</label>
+                <input v-model="input.code" type="text" />
+            </div>
+            <button @click="onThem" >Thêm</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data (){
+        return {
+            input : {
+                firstname: '',
+                lastname :'',
+                email :'',
+                code: ''
+            },
+        }
+    },
+    methods: {
+        onThem () {
+            // vì là component con, nên t truyền qua cha
+            // giá trị là this.input , đặt tên là input
+            this.$emit('input',this.input)
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/Todolist.vue
+++ src/views/Todolist.vue
@@ -1,32 +1,87 @@
 <template>
     <div>
-        <Table @naruto="nhanvao" :data="table"></Table>
-        <div>{{hello}}</div>
+        <AddTodo @input="giatriInput"></AddTodo>
+        <Table @item="nhanitemvao" @ID="componentninhanvao" @naruto="nhanvao" :data="table"></Table>
+        <div v-if="hienthi">
+            <label>Sửa</label>
+            <div style="display:flex;flex-flow:column wrap">
+                <div>
+                    <label>First Name</label>
+                    <input v-model="firstname" type="text" >
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <input v-model="lastname" type="text" >
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input v-model="email" type="text" >
+                </div>
+                <div>
+                    <label>Code</label>
+                    <input v-model="code" type="text" >
+                </div>
+                <button @click="suaxong">Sửa xong</button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import Table from './../components/table'
+import AddTodo from './../components/AddTodo'
 import { mapActions , mapGetters } from 'vuex'
 
 export default {
     data (){
         return {
-            hello : 'hello word',
+            firstname:'',
+            lastname:'',
+            email :'',
+            code :'',
+            id : 0,
             table : [
             ],
+            hienthi : false
         }
     },
     computed : {
         ...mapGetters(['getTable'])
     },
     methods : {
+        suaxong () {
+            this.Sua({
+                firstname : this.firstname,
+                lastname : this.lastname,
+                email : this.email,
+                code : this.code,
+                id : this.id
+            })
+        },
+        nhanitemvao (item) {
+            this.hienthi = true
+            this.firstname = item.FistName
+            this.lastname = item.LastName 
+            this.email = item['E-Mail']
+            this.code = item.Code
+            this.id = item.id
+        },
+        giatriInput (giatri) {
+            // gia tri nhận ở đây
+            // nhận xog t gọi 1 action từ mapActions
+            // t truyền giatri vô
+            this.Add(giatri)
+        },
+        componentninhanvao (id) {
+            this.Remove(id)
+        },
         nhanvao (value) {
             console.log(value)
         },
-        ...mapActions(['FetchData','lamgido'])
+        ...mapActions(['FetchData','Remove','Add','Sua'])
     },
     components: {
-        Table
+        Table,
+        AddTodo
     },
     watch : {
         getTable : function (val){
@@ -38,7 +93,6 @@ export default {
     created(){
         // hàm này sau khi create xong observa data
         this.FetchData()
-        this.lamgido()
     }
 }
 </script>
